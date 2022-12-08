c64a76b5c89ab94ba17251a7fd5e9484a221cc3c
joan rosario <joejoejoe100.jj@gmail.com>
2022-12-08 18:24:20 +0530

    added hasing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+
+const md5 = require('md5');
 
 const app = express();
 
@@ -20,7 +21,6 @@ const userSchema = new mongoose.Schema( {
 
 
 
-userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields:["password"] });
 const User = mongoose.model("User",userSchema);
 app.get("/",function(req,res){
   res.render("home");
@@ -36,7 +36,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
 const newUser = new User({
   email:req.body.username,
-  password:req.body.password
+  password:md5(req.body.password)
 });
 
 newUser.save(function(err){
@@ -50,7 +50,7 @@ newUser.save(function(err){
 });
 app.post("/login",function(req,res){
   const user =req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
   User.findOne({email:req.body.username},function(err,found){
     if(err){
       console.log(err);
